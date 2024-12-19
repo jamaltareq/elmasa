c9d7750557502087311598b394b67bfb2a5b7961
gamal-tarek <141655059+gamal-tarek@users.noreply.github.com>
2024-12-19 04:34:07 +0200

    Added

--- src/Components/Top/Top.jsx
+++ src/Components/Top/Top.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
 export default function Top() {
     const [isVisible, setIsVisible] = useState(false);
@@ -27,30 +27,68 @@ export default function Top() {
         });
     };
 
+    const openWhatsApp = () => {
+        window.open('https://wa.me/+966544442552', '_blank');
+    };
+
     return (
         <>
+            <button
+                onClick={openWhatsApp}
+                style={{
+                    position: 'fixed',
+                    bottom: '20px',
+                    right: '20px',
+                    fontSize: '16px',
+                    width: '60px',
+                    height: '60px',
+                    borderRadius: '50%',
+                    backgroundColor: '#25D366',
+                    color: '#fff',
+                    border: 'none',
+                    cursor: 'pointer',
+                    zIndex: '1000',
+                    animation: 'ring 2s infinite',
+                }}
+            >
+                <i className="fa-brands fs-2 fa-whatsapp"></i>
+            </button>
+
             {isVisible && (
                 <button
                     onClick={scrollToTop}
                     style={{
                         position: 'fixed',
-                        bottom: '20px',
+                        bottom: '90px',
                         right: '20px',
                         fontSize: '16px',
                         width: '50px',
                         height: '50px',
-                        borderRadius: '5px',
+                        borderRadius: '50%',
                         backgroundColor: '#000',
                         color: '#fff',
                         border: 'none',
                         cursor: 'pointer',
                         zIndex: '1000',
-                        borderRadius: '50%',
                     }}
                 >
                     <i className="fa-solid fa-chevron-up"></i>
                 </button>
             )}
+
+            <style jsx>{`
+                @keyframes ring {
+                    0% {
+                        box-shadow: 0 0 0 0 rgba(37, 211, 102, 0.5);
+                    }
+                    50% {
+                        box-shadow: 0 0 0 10px rgba(37, 211, 102, 0);
+                    }
+                    100% {
+                        box-shadow: 0 0 0 0 rgba(37, 211, 102, 0);
+                    }
+                }
+            `}</style>
         </>
     );
 }
